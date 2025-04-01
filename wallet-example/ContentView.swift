a0806e594922f34714ff6f635c09ef3ffcafa06f
DariaKozlovska <odriy.lovska@gmail.com>
2025-04-01 19:59:52 +0200

    Initial Commit

---
+++ wallet-example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wallet-example
+//
+//  Created by Daria Kozlovska on 01/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wallet-example/wallet_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallet_exampleApp.swift
+//  wallet-example
+//
+//  Created by Daria Kozlovska on 01/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct wallet_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
